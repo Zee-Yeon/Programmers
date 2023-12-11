ef2c940684751a25942c3a801e0c3f0c46ddf45e
Zee-Yeon <122084111+Zee-Yeon@users.noreply.github.com>
2023-12-11 22:29:32 +0900

    [level 2] Title: 숫자의 표현, Time: 1.53 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        int count = 1;
+        
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                sum += j;
+                if (sum == n) {                 
+                    count++;
+                    break;
+                }
+                if (sum > n) {
+                    break;
+                }
+            }
+            sum = 0;
+        }     
+        return count;
+    }
+}
